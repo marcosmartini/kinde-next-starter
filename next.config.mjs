db8deaf3ea8f79fe16fb1eba027050edc1d4f61f
Marcos Martini <marcosmartini7@gmail.com>
2024-10-14 13:19:28 +1000

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,17 @@
 const nextConfig = {
   env: {
     KINDE_SITE_URL:
-      process.env.KINDE_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+      process.env.KINDE_SITE_URL ?? process.env.VERCEL_ENV === "production"
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+        : `https://${process.env.VERCEL_URL}`,
     KINDE_POST_LOGOUT_REDIRECT_URL:
-      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? process.env.VERCEL_ENV === "production"
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+        : `https://${process.env.VERCEL_URL}`,
     KINDE_POST_LOGIN_REDIRECT_URL:
-      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}/dashboard`
-        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/dashboard`,
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? process.env.VERCEL_ENV === "production"
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/dashboard`
+        : `https://${process.env.VERCEL_URL}/dashboard`,
   },
 };
 
