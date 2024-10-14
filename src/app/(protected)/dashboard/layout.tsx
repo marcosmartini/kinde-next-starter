1ea2b94130ae09b8540d518d295184eba86d8485
Marcos Martini <marcosmartini7@gmail.com>
2024-10-14 16:40:34 +1000

    wip

--- src/app/(protected)/dashboard/layout.tsx
+++ src/app/(protected)/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Header from "@/components/Header";
+import Header from "@/components/MainHeader";
 import "../../globals.css";
 
 const geistSans = localFont({
--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "@/components/MainHeader";
 
 export default async function HomeHeader() {
   return <Header showNav={false} />;
