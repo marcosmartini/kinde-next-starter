6a426e3137a09effb0ec836a7495436449405a97
Marcos Martini <marcosmartini7@gmail.com>
2024-10-14 11:40:27 +1000

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ?? process.env.VERCEL_ENV === "production"
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+        : `https://${process.env.VERCEL_URL}`,
     KINDE_POST_LOGOUT_REDIRECT_URL:
-      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? process.env.VERCEL_ENV === "production"
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+        : `https://${process.env.VERCEL_URL}`,
     KINDE_POST_LOGIN_REDIRECT_URL:
-      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/dashboard`,
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? process.env.VERCEL_ENV === "production"
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/dashboard`
+        : `https://${process.env.VERCEL_URL}/dashboard`,
   },
 };
 
